7a2eba63b5c1628ad9e26294cc01d8227245d7b5
rabbit <d.mamonov@nekrasovka.ru>
2024-04-18 14:40:30 +0300

    add go to lift

--- src/components/Mylift.vue
+++ src/components/Mylift.vue
@@ -4,12 +4,23 @@
   <div class="mycontent">
     <div class="lift">
       <div class="etag">
-        <div class="block"></div>
+        <div
+          class="block"
+          :style="{ bottom: positionnow + 'px' }"
+          :class="{ block: true, goblock: isgoblock }"
+        ></div>
       </div>
       <div class="navigatelift">
         <div class="buttonetage" v-for="etg in numbersetag" :key="etg">
-          <div>{{ etg }}</div>
-          <button-lift><div class="circle"></div></button-lift>
+          <div class="chislo">{{ etg }}</div>
+          <button-lift
+            @click="
+              {
+                GoLift(etg);
+              }
+            "
+            ><div class="circle"></div
+          ></button-lift>
         </div>
       </div>
     </div>
@@ -24,7 +35,58 @@ export default {
     return {
       numbersetag: 5,
       positionnow: 0,
+      buttonclick: false,
+      isgoblock: false,
+      isogidanie: false,
+      nowetag: 1,
+      ochered: [],
     };
+  },
+
+  methods: {
+    async GoLift(etg) {
+      if (this.ochered.includes(etg)) {
+        return;
+      }
+
+      this.ochered.push(etg);
+      await this.gotolift();
+    },
+
+    async gotolift() {
+      if (this.isgoblock || this.ochered.length === 0) {
+        return;
+      }
+
+      this.isgoblock = true;
+      if (this.isogidanie) {
+        if (this.ochered.includes(ocher)) {
+          this.isgoblock = false;
+          return;
+        }
+      }
+
+      const ocher = this.ochered[0];
+      const distance = Math.abs(ocher - this.nowetag);
+      const distantion = distance * 1000;
+
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          this.nowetag = ocher;
+          this.positionnow = (ocher - 1) * 100;
+          resolve();
+        }, distantion);
+      });
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, distantion + 3000);
+      });
+
+      this.ochered.shift();
+
+      this.isgoblock = false;
+      await this.gotolift();
+    },
   },
 };
 </script>
@@ -38,7 +100,7 @@ export default {
 }
 .lift {
   width: 600px;
-  height: 700px;
+  height: 500px;
   display: flex;
 
   justify-content: space-around;
@@ -49,13 +111,21 @@ export default {
   height: 100%;
   border: 2px solid #000;
   display: flex;
-  align-items: end;
+
+  position: relative;
 }
 
 .block {
+  position: absolute;
   width: 100%;
   height: 100px;
   background-color: aqua;
+  bottom: 0;
+  transition: bottom 4s ease-in-out;
+}
+
+.goblock {
+  transition: bottom 4s ease-in-out;
 }
 
 .circle {
